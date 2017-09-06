2a337772d48395958a714c91b986f80ae81622a5
Lexa <Lexa@Lexa-LT>
2017-09-07 04:34:33 +1000

    Добавлены cors

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace app\controllers;
 
 use yii\rest\ActiveController;
+use yii\filters\Cors;
 
 /**
  * Поле Пользователи
@@ -12,4 +13,21 @@ use yii\rest\ActiveController;
 class UserController extends ActiveController {
     
     public $modelClass = 'app\models\User';
+    
+    public function behaviors(){
+        $behaviors = parent::behaviors();
+        
+        $behaviors['corsFilter'] = [
+            'class' => Cors::className(),
+            'cors' => [
+                    'Origin' => ['*'],
+                    'Access-Control-Request-Method' => ['GET', 'OPTIONS'],
+                    'Access-Control-Request-Headers' => ['*'],
+                    'Access-Control-Expose-Headers' => ['X-Pagination-Current-Page', 'X-Pagination-Page-Count'],
+                ],
+        ];        
+       
+        return $behaviors;
+        
+    }
 }
